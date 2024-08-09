520ac829469d280a74e0ad07b0c4799084ade9e2
OckTheOctopus <Yaseen6607@yahoo.com>
2024-08-09 14:14:17 +1000

    Updated motor code

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,17 +1,26 @@
-const uint8_t left_motor = 13;
-const uint8_t right_motor = 14;
+
+const int left_motor = 12;
+const int right_motor = 13;
 
 /*
 @params void
 @returns void
 */
 void setup() {
-    Serial.begin(9600);
-    digitalWrite(left_motor, HIGH);
-    digitalWrite(right_motor, HIGH);
+    Serial.begin(115200);
+    pinMode(left_motor, OUTPUT);
+    pinMode(right_motor, OUTPUT);
+
 }
 
 void loop() {
     // Yaseen was here
     // hii
+    digitalWrite(left_motor, HIGH);
+    digitalWrite(right_motor, HIGH);
+    Serial.println("Motor on high voltage");
+    delay(2000);
+    digitalWrite(left_motor, LOW);
+    digitalWrite(right_motor, LOW);
+    Serial.println("Motor on low voltage");
 }
