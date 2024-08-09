ccdeea4fe28cb2187d6a2e80233dc7749dfee0fd
henos <65333161+henos1029@users.noreply.github.com>
2024-08-09 14:03:45 +1000

    motor stuff init

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,6 +1,14 @@
+const uint8_t left_motor = 13;
+const uint8_t right_motor = 14;
+
+/*
+@params void
+@returns void
+*/
 void setup() {
     Serial.begin(9600);
-    
+    digitalWrite(left_motor, HIGH);
+    digitalWrite(right_motor, HIGH);
 }
 
 void loop() {
