4b3afc97c78cc791286f9d245cd4477fc9a551d8
henos <65333161+henos1029@users.noreply.github.com>
2024-08-09 15:25:38 +1000

    motors spin

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,26 +1,36 @@
+#include <Servo.h>
 
-const int left_motor = 12;
-const int right_motor = 13;
+const uint8_t MAX_SPEED = 18;
 
-/*
-@params void
-@returns void
-*/
+// motors
+Servo left_motor;
+Servo right_motor;
+
+// distance sensor
+const uint8_t trig = 3;
+const uint8_t echo = 3;
+
 void setup() {
     Serial.begin(115200);
-    pinMode(left_motor, OUTPUT);
-    pinMode(right_motor, OUTPUT);
-
+    left_motor.attach(12);
+    right_motor.attach(13);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, OUTPUT);
 }
 
 void loop() {
-    // Yaseen was here
-    // hii
-    digitalWrite(left_motor, HIGH);
-    digitalWrite(right_motor, HIGH);
-    Serial.println("Motor on high voltage");
+    for (uint8_t speed; speed < MAX_SPEED; speed++) {
+        left_motor.write(speed);
+        right_motor.write(speed);
+        Serial.println(speed);
+        delay(100);
+    }
+    // digitalWrite(2, HIGH);
+    Serial.println("Motors on");
+    left_motor.write(MAX_SPEED);
+    right_motor.write(MAX_SPEED);
     delay(2000);
-    digitalWrite(left_motor, LOW);
-    digitalWrite(right_motor, LOW);
-    Serial.println("Motor on low voltage");
+    Serial.println("Motors off");
+    left_motor.write(0);
+    right_motor.write(0);
 }
