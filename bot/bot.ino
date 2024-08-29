2c22742cd5fdcd611acf5bb58ea6297f0117d7d3
henos <65333161+henos1029@users.noreply.github.com>
2024-08-30 09:29:13 +1000

    no comments allowed

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,43 +1,5 @@
 #include <HCSR04.h>
-
-#include <Arduino.h>
-#include <ArduinoSTL.h>
-
-// #include <system_configuration.h>
-// #include <unwind-cxx.h>
-
 #include <Servo.h>
-
-// using namespace std;
-
-std::vector<int> j;
-
-
-int echo5 () {
-
-}
-
-int infared() {
-    
-}
-
-int floodfill() {
-    //we see a thing
-    //turn to the thing
-    //move forward until one of 2 things
-    //thing disappears or obstacle see in path
-    //if obstacle see in path
-    //  rotate left until obstacle no more
-    //  rotate right until thing no more
-    //  consider furthest poitn that is closest to target
-    //  need SA
-    //  rotate and move until inline obstacle 
-
-    return 0;
-}
-
-const uint8_t BAUD_RATE = 115200;
-const uint8_t MAX_SPEED = 18;
 
 // motors
 Servo left_motor;
@@ -50,33 +12,18 @@ const uint8_t trig = 11;
 const uint8_t echo = 10;
 UltraSonicDistanceSensor distSensor = UltraSonicDistanceSensor(trig, echo);
 
+void print_dist(float dist) {
+    Serial.print("Distance: ");
+    Serial.print(dist);
+    Serial.print("\n");
+}
 
 void setup() {
-    left_motor.attach(left_motor_pin);
-    right_motor.attach(right_motor_pin);
-    pinMode(trig, OUTPUT);
-    pinMode(echo, INPUT);
-    Serial.begin(BAUD_RATE);
-    Serial.println("Setup complete");
+  Serial.begin(9600);
+  Serial.println("Setup start");
+  Serial.println("Setup complete\n");
 }
 
 void loop() {
-    // // speeds up
-    // for (int speed; speed < MAX_SPEED; speed++) {
-    //     left_motor.write(speed);
-    //     right_motor.write(speed);
-    //     Serial.println(speed);
-    //     delay(100);
-    // }
-    // // digitalWrite(2, HIGH);
-    // Serial.println("Motors on");
-    // left_motor.write(MAX_SPEED);
-    // right_motor.write(MAX_SPEED);
-    // delay(2000);
-    // Serial.println("Motors off");
-    // left_motor.write(0);
-    // right_motor.write(0);
-
-    Serial.print("Distance: ");
-    Serial.print(distSensor.measureDistanceCm());
+  print_dist(distSensor.measureDistanceCm());
 }
