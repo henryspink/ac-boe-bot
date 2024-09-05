ffe4145e706984c9f52c8b0de456e2f8b2e7ffd9
OckTheOctopus <Yaseen6607@yahoo.com>
2024-09-05 10:43:00 +1000

    skibidi

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,6 +1,7 @@
 #include <HCSR04.h>
 #include <Servo.h>
 
+const uint16_t BAUD_RATE = 115200;
 const uint16_t distGround = /* !find value! temp: */ 400;
 
 // motors
@@ -29,11 +30,11 @@ struct Instruction {
         float heading;
 };
 
-void print_dist(float dist) {
-    Serial.print("Distance: ");
-    Serial.print(dist);
-    Serial.print("\n");
-}
+// void print_dist(float dist) {
+//     Serial.print("Distance: ");
+//     Serial.print(dist);
+//     Serial.print("\n");
+// }
 
 void movement(State state, Instruction instruction) {
     return;
@@ -56,11 +57,11 @@ State findHeading(State state) {
         }
     }
     for (int i = 0; i < leftSideRotations; i++) {
-        right_motor.write(180);
+        right_motor.write(-180);
     }
     while (((currentDist <= currentDist * 1.1) && (currentDist >= currentDist * 0.9)) || (currentDist == -1)) { // 10% tolerance
         if (currentDist != -1) {
-            right_motor.write(180);
+            right_motor.write(-180);
             currentDist = distSensor.measureDistanceCm();
             rightSideRotations++;
         }
@@ -72,7 +73,7 @@ State findHeading(State state) {
         }
         while (((currentDist <= currentDist * 1.1) && (currentDist >= currentDist * 0.9)) || (currentDist == -1)) { // 10% tolerance
             if (currentDist != -1) {
-                left_motor.write(180);
+                left_motor.write(-180);
                 currentDist = distSensor.measureDistanceCm();
                 leftSideRotations++;
             }
@@ -94,33 +95,34 @@ State findHeading(State state) {
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Setup start");
+    Serial.begin(BAUD_RATE);
+    // Serial.println("Setup start");
     left_motor.attach(left_motor_pin);
     right_motor.attach(right_motor_pin);
-    Serial.println("Setup complete");
+    // Serial.println("Setup complete");
 }
 
 void loop() {
+    // left_motor.write(180);
+    // right_motor.write(-180);
     float dist = distSensor.measureDistanceCm();
+    // Serial.print(dist);
     State state {
         .dist = dist,
         .heading = heading,
         .speed = 0
     };
     if (dist < 0) {
-        Serial.println("Distance error");
+        // Serial.println("Distance error");
     } else if (dist < distGround+10) {
-        print_dist(dist);
+        // print_dist(dist);
         state = findHeading(state);
-        left_motor.write(180);
-        right_motor.write(180);
     } else {
         Instruction instruction {
             .speed = 1,
             .heading = 0
         };
-        Serial.println(instruction.speed);
+        // Serial.println(instruction.speed);
         movement(state, instruction);
     }
 }
