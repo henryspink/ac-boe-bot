acfbaea57329808bf26e03183b12fed5ac220652
henos <65333161+henos1029@users.noreply.github.com>
2024-09-15 11:39:18 +1000

    not working :)

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,30 +1,38 @@
 #include <HCSR04.h>
 #include <Servo.h>
 
-
-// 360 time
-
-const uint16_t distGround = /* !find value! temp: */ 400;
-const int BAUD_RATE = 115200;
+#define BAUD_RATE 115200
 
 // motors
+Servo ir_motor;
 Servo left_motor;
-const uint8_t left_motor_pin = 12;
 Servo right_motor;
-const uint8_t right_motor_pin = 13;
+#define IR_MOTOR_PIN 11
+#define LEFT_MOTOR_PIN 12
+#define RIGHT_MOTOR_PIN 13
 
 // distance sensor
-const uint8_t trig = 11;
-const uint8_t echo = 10;
-UltraSonicDistanceSensor distSensor = UltraSonicDistanceSensor(trig, echo);
+#define DIST_THRESH 20
+#define TRIGGER_PIN 4
+#define ECHO_PIN 3
+UltraSonicDistanceSensor distSensor = UltraSonicDistanceSensor(TRIGGER_PIN, ECHO_PIN);
+
+// #define BAUD_RATE 115200
+
+// namespace math {
+//     #include <cmath>
+// }
+
+
 
 float heading = 0;
 
 struct State {
     public:
-        float dist;
-        float heading;
-        float speed;
+        float dist;         /* cm               */
+        float heading;      /* deg 0 -> 360     */
+        float speed;        /* -180 -> 180      */
+        float ir_rotations; /* deg -90 -> 90    */
 };
 
 struct Instruction {
@@ -34,40 +42,57 @@ struct Instruction {
 };
 
 
-const Instruction FORWARDS_MAX = Instruction {
-    .speed = 180,
-    .heading = 0
-};
+namespace DIRECTION {
+    const Instruction FORWARDS = Instruction {
+        .speed = 180,
+        .heading = 0
+    };
 
-const Instruction BACKWARDS_MAX = Instruction {
-    .speed = -180,
-    .heading = 0
-};
+    const Instruction BACKWARDS = Instruction {
+        .speed = -180,
+        .heading = 0
+    };
 
-const Instruction STOP = Instruction {
-    .speed = 0,
-    .heading = 0
-};
+    const Instruction STOP = Instruction {
+        .speed = 0,
+        .heading = 0
+    };
+}
 
-// void print_dist(float dist) {
-//     Serial.print("Distance: ");
-//     Serial.print(dist);
-//     Serial.print("\n");
-// }
+void print_dist(float dist) {
+    dist > 0 ? Serial.print("Distance: ") : Serial.print("Distance error: ");
+    Serial.print(dist);
+    Serial.print("\n");
+}
 
 void movement(State state, Instruction instruction) {
     if (instruction.heading != 0) {
         // heading = state.heading + instruction.heading;
     }
     left_motor.write(-instruction.speed);
-    left_motor.write(instruction.speed);
+    right_motor.write(instruction.speed);
 }
 
-void infrared(State state) {
-    return;
+State infrared(State state) {
+    //! TODO: find rotations amount
+    if (state.ir_rotations <= 4) {
+        ir_motor.write(180);
+        delay(50);
+        ir_motor.write(0);
+    } else {
+        ir_motor.write(-180);
+        delay(50);
+        ir_motor.write(0);
+    }
+    return State {
+        .dist = state.dist,
+        .heading = state.heading,
+        .speed = state.speed,
+        .ir_rotations = state.ir_rotations++
+    };
 }
 
-State findHeading(State state) {
+State dodge_object(State state) {
     float initialDist = state.dist;
     float currentDist = initialDist;
     int rotations = 0;
@@ -111,42 +136,32 @@ State findHeading(State state) {
     return State {
         .dist = currentDist,
         .heading = state.heading + (direction * rotations),
-        .speed = state.speed
+        .speed = state.speed,
+        .ir_rotations = state.ir_rotations,
     };
 }
 
 void setup() {
-    Serial.begin(9600);
-    // Serial.println("Setup start");
-    left_motor.attach(left_motor_pin);
-    right_motor.attach(right_motor_pin);
-    // Serial.println("Setup complete");
+    Serial.begin(BAUD_RATE);
+    Serial.println("Setup start");
+    ir_motor.attach(IR_MOTOR_PIN);
+    left_motor.attach(LEFT_MOTOR_PIN);
+    right_motor.attach(RIGHT_MOTOR_PIN);
+    Serial.println("Setup complete");
 }
 
 void loop() {
-    // left_motor.write(180);
-    // right_motor.write(-180);
     float dist = distSensor.measureDistanceCm();
-    Serial.println(dist);
+    print_dist(dist);
     State state {
         .dist = dist,
         .heading = heading,
         .speed = 0
     };
-    if (dist < 0) {
-        // Serial.println("Distance error");
-    } else if (dist < distGround+10) {
-        // Serial.println(dist);
-        state = findHeading(state);
-        left_motor.write(-180);
-        right_motor.write(180);
-        // movement(state, )
+    if ((round(dist) < DIST_THRESH) && (state.dist > 0)) {
+        state = dodge_object(state);
+        movement(state, DIRECTION::FORWARDS);
     } else {
-        Instruction instruction {
-            .speed = 1,
-            .heading = 0
-        };
-        // Serial.println(instruction.speed);
-        movement(state, instruction);
+        movement(state, DIRECTION::FORWARDS);
     }
 }
--- bot/test.ino
+++ bot/test.ino
@@ -1,17 +1,81 @@
+// // #include <HCSR04.h>
 // #include <Servo.h>
 
-// Servo left_motor;
-// const uint8_t left_motor_pin = 12;
-// Servo right_motor;
-// const uint8_t right_motor_pin = 13;
-// uint16_t time = 0;
+// // const uint8_t led = 6;
+
+// // const uint8_t trig = 4;
+// // const uint8_t echo = 3;
+// // UltraSonicDistanceSensor distSensor = UltraSonicDistanceSensor(trig, echo);
+
+// // const uint8_t ir = 2;
+
+// Servo ir_motor;
+// uint8_t ir_motor_pin = 11;
+
+// // int ir_rotations = 0;
+// // int sign = -1;
 
 // void setup() {
 //     Serial.begin(9600);
 //     Serial.println("Setup start");
-//     left_motor.attach(left_motor_pin);
-//     right_motor.attach(right_motor_pin);
+//     // pinMode(led, OUTPUT);
+//     // pinMode(ir, INPUT);
+//     ir_motor.attach(ir_motor_pin);
 //     Serial.println("Setup complete");
+// }
 
-//     left_motor.write(180);
+// void loop() {
+//     // float dist = distSensor.measureDistanceCm();
+//     // bool ir_signal = digitalRead(ir);
+//     // // Serial.println(dist);
+//     // // if (dist < 0) {
+//     // //     digitalWrite(led, LOW);
+//     // // } else {
+//     // //     digitalWrite(led, HIGH);
+//     // // }
+//     // // Serial.println(ir_signal);
+//     // digitalWrite(led, !ir_signal);
+
+//     // //! TODO: find rotations amount
+
+
+//     // Serial.println(sign);
+//     // Serial.println(50*sign);
+//     // ir_motor.write(50*sign);
+//     // delay(10);
+//     // ir_motor.write(0);
+//     // ir_rotations += sign;
+//     // Serial.println(ir_rotations);
+//     // // if (ir_rotations >= 4 || ir_rotations <= -4) sign = -sign;
+//     // if (ir_rotations >= 4) {
+//     //   sign = -1;
+//     // } else if (ir_rotations <= -4) {
+//     //   sign = 1;
+//     // }
+//     // delay(1000);
+//     // // Serial.println(sign);
+
+//     // // if (ir_rotations >= 0 && ir_rotations < 4) {
+//     // //     ir_motor.write(180);
+//     // //     delay(50);
+//     // //     ir_motor.write(0);
+//     // //     ir_rotations--;
+//     // // } else if (ir_rotations <= 0 && ir_rotations > -4){
+//     // //     ir_motor.write(-180);
+//     // //     delay(50);
+//     // //     ir_motor.write(0);
+//     // //     ir_rotations++;
+//     // // } else {
+//     // //   Serial.println("die");
+//     // // }
+//     ir_motor.write(50); // Set speed
+//     delay(1000); // Run for 1 second
+//     ir_motor.write(0); // Stop motor
+//     delay(500); // Wait for 0.5 seconds
+//     Serial.println("hi");
+//     // Move backward
+//     ir_motor.write(-50); // Set speed
+//     delay(1000); // Run for 1 second
+//     ir_motor.write(0); // Stop motor
+//     delay(500); // Wait for 0.5 second
 // }
